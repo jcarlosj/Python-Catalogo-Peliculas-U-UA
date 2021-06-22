dfb5cbc3f2cfc3a885ed43f58ec89de458d7a136
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-06-21 21:15:27 -0500

    [+] main.py: Funciones anidadas & reemplazo de condicional if-else por dict

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from domain.Movie import Movie
 from service.Catalog import Catalog
+import sys
 
-option = None
+def main() :
 
-while option != 4 :
+    option = None
 
-    try:
-        print( 'Catalogo de películas' .center( 30, '-' ) )
-        print( 'Menu \n  1. Agregar película\n  2. Listar películas\n  3. Eliminar archivo\n  4. Salir \n' )
-        option = int( input( 'Escribe tu opción (1-4): ' ) )
+    def add_movie() :
+        movie_name = input( 'Nombre de la película: ' )
+        movie = Movie( movie_name )
+        Catalog .add( movie )
 
-        if option == 1 :
-            movie_name = input( 'Nombre de la película: ' )
-            movie = Movie( movie_name )
-            Catalog .add( movie )
+    def list_movies() :
+        Catalog .list()
 
-        elif option == 2 :
-            Catalog .list()
-        elif option == 3 :
-            Catalog .delete()
+    def delete_file() :
+        Catalog .delete()
 
-    except Exception as e:
-        print( f'Ocurrió un error\n  > { e }\n' )
-        option = None
+    def exit() :
+        return 4
 
-else :
-    print( 'El programa a finalizado su ejecución' )
+    while option != 4 :
+
+        try:
+            print( 'Catalogo de películas' .center( 30, '-' ) )
+            print( 'Menu \n  1. Agregar película\n  2. Listar películas\n  3. Eliminar archivo\n  4. Salir \n' )
+            option = int( input( 'Escribe tu opción (1-4): ' ) )
+
+            options = {
+                1: add_movie,
+                2: list_movies,
+                3: delete_file,
+                4: exit
+            }
+
+            # print( type( options ) )      # <class 'dict'>
+            option = options[ option ]()
+
+        except Exception as e:
+            print( f'Ocurrió un error\n  > { e }\n' )
+            option = None
+
+    else :
+        print( 'El programa a finalizado su ejecución' )
+
+main()
